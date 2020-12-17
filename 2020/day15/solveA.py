a3ee413a624f23308cc2655af262b74c9964c9f9
Sajid Mehmood <sajid.mehmood@datadoghq.com>
2020-12-16 21:06:34 -0500

    2020 day15

---
+++ 2020/day15/solveA.py
@@ -0,0 +1,18 @@
+import sys
+
+def find_nth(starting, n):
+    last_turn = {num: i for i, num in enumerate(starting)}
+    last_spoken = None
+    for i in range(len(starting), n):
+        ith = 0 if last_spoken not in last_turn else (i-1) - last_turn[last_spoken]
+        last_turn[last_spoken] = i-1
+        last_spoken = ith
+
+    return ith
+
+def main():
+    starting = [int(x) for x in sys.stdin.readline().split(',')]
+    print(find_nth(starting, 2020))
+
+if __name__ == '__main__':
+    main()
